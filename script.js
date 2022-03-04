2cd93e22544f8d9ab5c8c3780ed0ef82fd11a342
Roman Vinokurov <roman.vinokurov96@gmail.com>
2022-03-04 12:37:59 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    content.classList.toggle("hidden")
+  });
+}
